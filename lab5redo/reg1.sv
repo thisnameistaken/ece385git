1c7a368e2414862a36ce38c9723d2d3049f98563
unknown <smakani2@illinois.edu>
2020-02-21 01:51:46 -0600

    still broken

---
+++ lab5redo/reg1.sv
@@ -0,0 +1,18 @@
+module reg_1 (input  logic Clk, Reset, Load, D, 
+              output logic Data_Out);
+
+    always_ff @ (posedge Clk)
+    begin
+	 	 if (Reset) //notice, this is a sycnrhonous reset, which is recommended on the FPGA
+			  Data_Out <= 1'h0;
+		 else if (Load)
+			  Data_Out <= D;
+		 else
+		 begin
+			  //concatenate shifted in data to the previous left-most 3 bits
+			  //note this works because we are in always_ff procedure block
+			  Data_Out <= Data_Out; 
+	    end
+    end
+
+endmodule 
