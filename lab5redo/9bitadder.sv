b05ab3e27cf1605d2213adb5cd55404601daffe4
unknown <smakani2@illinois.edu>
2020-02-20 23:39:39 -0600

    redoing everything

---
+++ lab5redo/9bitadder.sv
@@ -0,0 +1,22 @@
+module carry_select_adder
+(
+    input   logic[8:0]     A,
+    input   logic[8:0]     B,
+    output  logic[8:0]     Sum,
+    output  logic          Carry
+);
+
+    /* TODO
+     *
+     * Insert code here to implement a carry select.
+     * Your code should be completly combinational (don't use always_ff or always_latch).
+     * Feel free to create sub-modules or other files. */
+	  
+	  logic C0, C1;
+	  csa0 FA0(.A(A[3:0]), .B(B[3:0]), .cIn(0), .out(Sum[3:0]), .cout(C0));
+	  csa FA1(.A(A[7:4]), .B(B[7:4]), .cIn(C0), .out(Sum[7:4]), .cout(C1));
+	  full_adder FA2(.x(A[8]), .y(B[8]), .cIn(c1), .s(Sum[8]), .cOut(Carry));
+	  
+	  
+	    
+endmodule
---
+++ lab5redo/mult.sv
@@ -0,0 +1,20 @@
+module mult
+(
+    input   logic           Clk,        // 50MHz clock is only used to get timing estimate data
+    input   logic           Reset,      // From push-button 0.  Remember the button is active low (0 when pressed)
+    input   logic           ClearA_LoadB,      // From push-button 1
+    input   logic           Run,        // From push-button 3.
+    input   logic[7:0]      S,         // From slider switches
+    
+    // all outputs are registered
+           
+    output  logic[6:0]      AhexU,      
+    output  logic[6:0]      AhexL,
+    output  logic[6:0]      BhexU,
+    output  logic[6:0]      BhexL,
+    output  logic[7:0]      Aval,
+    output  logic[7:0]      Bval,
+	 output  logic           X
+);
+
+endmodule 
