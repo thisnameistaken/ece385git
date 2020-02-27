7acdb6b352f9bf1542d4f8f1273600ca70d2b035
unknown <smakani2@illinois.edu>
2020-02-26 20:37:21 -0600

    mux

--- lab6/mux.sv
+++ lab6/mux.sv
@@ -2,17 +2,57 @@ module twomux(input logic A, B, Select, output logic Out);
 		
 		always_comb begin
 			if(Select == 1'b0) begin
-				out = A;
+				Out = A;
 			end
 			
 			else begin
-				out = B;
+				Out = B;
 			end
-		
-		
-		
 		
 		end
 
+endmodule
+
+
+module fourmux(input logic A, B, C, D, input logic [1:0] Select, output logic Out);
+		
+		always_comb begin
+			if(Select == 2'b00) begin
+				Out = A;
+			end
+			
+			else if(Select == 2'b01) begin
+				Out = B;
+			end
+			
+			else if(Select == 2'b10) begin
+				Out = C;
+			end
+			
+			else begin
+				Out = D;
+			end
+		end
 
 endmodule
+
+
+
+module threemux(input logic A, B, C, input logic [1:0] Select, output logic Out);
+		
+		always_comb begin
+			if(Select == 2'b00) begin
+				Out = A;
+			end
+			
+			else if(Select == 2'b01) begin
+				Out = B;
+			end
+			
+			else begin
+				Out = C;
+			end
+		end
+
+endmodule
+
