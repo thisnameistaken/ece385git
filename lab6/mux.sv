40915d8d9ff3b5dea7f0178769a777937f8fa245
unknown <smakani2@illinois.edu>
2020-02-26 20:23:10 -0600

    mux

--- lab6/mux.sv
+++ lab6/mux.sv
@@ -1,1 +1,18 @@
-s
+module twomux(input logic A, B, Select, output logic Out);
+		
+		always_comb begin
+			if(Select == 1'b0) begin
+				out = A;
+			end
+			
+			else begin
+				out = B;
+			end
+		
+		
+		
+		
+		end
+
+
+endmodule
